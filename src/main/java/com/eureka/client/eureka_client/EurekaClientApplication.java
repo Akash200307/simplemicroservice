e5ee945f57e066ff5956979a82a0da8191acc7e5
Akash <akash200307@outlook.com>
2024-06-23 21:33:25 +0530

    service

---
+++ src/main/java/com/eureka/client/eureka_client/EurekaClientApplication.java
@@ -0,0 +1,24 @@
+package com.eureka.client.eureka_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+	@Bean
+	public SimpleFilterProvider simplefilter(){
+		return new SimpleFilterProvider();
+	}
+
+
+}
